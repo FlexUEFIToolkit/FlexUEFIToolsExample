723125b9de92949afe3bd7e378150da43bce72a8
Rongchuan Liu <rongchuan_liu@sjtu.edu.cn>
2023-12-26 14:48:30 +0800

    add user code of FLASH READ

---
+++ FlexUEFIToolkit_lrc.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include "FlexUEFIToolkitDxe.h"
+
+#define MAX_PAYLOAD 1024
+
+#define MAX_PAYLOAD 1024 /* maximum payload size*/
+struct sockaddr_nl src_addr, dest_addr;
+struct nlmsghdr *nlh = NULL;
+struct iovec iov;
+int sock_fd;
+struct msghdr msg;
+
+typedef enum {
+    futNonOp            = 0,
+    futToLowerCase      = 1,
+    futReadFlash         =2,
+    futChangeBIOS	= 3,
+    futFuncNum
+
+} FLEX_UEFI_TOOKLIT_FUNC_TYPE;
+
+int main()
+{
+    sock_fd = socket(PF_NETLINK, SOCK_RAW, FLEX_UEFI_TOOLKIT_NETLINK_ID);
+    if (sock_fd < 0)
+        return -1;
+
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+    src_addr.nl_pid = getpid(); /* self pid */
+
+    bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
+
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 0; /* For Linux Kernel */
+    dest_addr.nl_groups = 0; /* unicast */
+
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    nlh->nlmsg_pid = getpid();
+    nlh->nlmsg_flags = 0;
+
+    FLEX_UEFI_TOOKLIT_FUNC_TYPE funcType = futReadFlash;
+    int lba = 0;
+    int offset = 0;
+    int readByte = 100;
+    char msgArg[100];
+    sprintf(msgArg,"%d,%d,%d,%d",(int)funcType,lba,offset,readByte);
+    strcpy((char *)NLMSG_DATA(nlh), msgArg);
+
+    iov.iov_base = (void *)nlh;
+    iov.iov_len = nlh->nlmsg_len;
+    msg.msg_name = (void *)&dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    printf("Sending message to kernel\n");
+    sendmsg(sock_fd, &msg, 0);
+    printf("Waiting for message from kernel\n");
+
+    /* Read message from kernel */
+    recvmsg(sock_fd, &msg, 0);
+    printf("Received message payload: %s\n", NLMSG_DATA(nlh));
+    close(sock_fd);
+}
